a9b5c7d058fc369faae052a7992d1e9c3091f6be
sumanChoi <voltovo87@gmail.com>
2020-01-10 19:04:55 +0900

    Refactor
    code 작성 완료 ( 작동 X )

--- src/java_Calculator/CalLogic.java
+++ src/java_Calculator/CalLogic.java
@@ -5,5 +5,9 @@ import java.util.StringTokenizer;
 public class CalLogic {
 	public double cal(String str) {
 		StringTokenizer t = new StringTokenizer(str, "+-*/", true);
+		while(t.hasMoreTokens()) {
+			System.out.println(t.nextToken());
+		}
+		return Double.parseDouble(t.toString());
 	}
 }
